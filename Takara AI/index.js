35c67eb4239bfbea1a2764035ac8add46f99439c
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2022-02-25 16:38:16 +0100

    :alien: Pawn movement fix #1

--- Takara AI/index.js
+++ Takara AI/index.js
@@ -27,18 +27,18 @@ const player_colours = [
 const random_player_colours = player_colours[Math.floor(Math.random() * player_colours.length)];
 
 const game = new Chessboard(document.getElementById("chessgame"), {
-	position: "start", 
-	orientation: random_player_colours, 
+	position: "start",
+	orientation: random_player_colours,
 	style: {
 		cssClass: "default",
-		showCoordinates: true, 
+		showCoordinates: true,
 		borderType: BORDER_TYPE.frame,
 		aspectRatio: 1,
 		moveFromMarker: MARKER_TYPE.markerDot,
 		moveToMarker: MARKER_TYPE.markerDot
 	},
 	responsive: true,
-	animationDuration: 300, 
+	animationDuration: 300,
 	sprite: {
 		url: "./assets/images/chessboard-sprite-staunty.svg",
 		// url: "./assets/images/chessboard-sprite.svg",
@@ -63,23 +63,37 @@ game.enableMoveInput((event) => {
 
 			console.log("Current position:", event.squareTo);
 
+			let SquareMoveFromLetter = event.squareFrom.charAt(0);
+			let SquareMoveToLetter = event.squareTo.charAt(0);
+			let SquareMoveFromInt = (event.squareFrom).match(/\d+/)[0];
+			let SquareMoveToInt = (event.squareTo).match(/\d+/)[0];
+
+			console.log("SquareFrom:", SquareMoveFromLetter);
+			console.log("SquareTo:", SquareMoveToLetter);
+			console.log("SquareFrom", SquareMoveFromInt);
+			console.log("SquareTo", SquareMoveToInt);
+			console.log("Difference:", SquareMoveFromInt - SquareMoveToInt);
+
 			if (random_player_colours === COLOR.white) { // if white
 
 				if (game.getPiece(event.squareFrom).includes("p")) { // if piece is pawn
 
-					let SquareMoveFromLetter = event.squareFrom.charAt(0);
-					let SquareMoveToLetter = event.squareTo.charAt(0);
-					let SquareMoveFromInt = (event.squareFrom).match(/\d+/)[0];
-					let SquareMoveToInt = (event.squareTo).match(/\d+/)[0];
+					if (SquareMoveFromInt = 2) { //TODO: Make sure you can only move 2 squares if you are on start, its possible to skip any on the same row rn.
 
-					console.log("SquareFrom:", SquareMoveFromLetter);
-					console.log("SquareTo:", SquareMoveToLetter);
-					console.log("SquareFrom", SquareMoveFromInt);
-					console.log("SquareTo", SquareMoveToInt);
+						console.log('wowzers 1');
 
-					if (SquareMoveFromInt != 2) { //TODO: Make sure you can only move 2 squares if you are on start, its possible to skip any on the same row rn.
+						if ((SquareMoveFromInt - SquareMoveToInt) < -2) {
 
-						if (SquareMoveFromInt - SquareMoveToInt != -1) { // you cant skip more than 2 fields
+							console.log("Illegal pawn movement");
+							return false;
+
+						};
+
+					} else if (SquareMoveFromInt != 2) {
+
+						console.log('wowzers 2');
+
+						if ((SquareMoveFromInt - SquareMoveToInt) != -1) { // you cant skip more than 2 fields
 
 							console.log("Illegal pawn movement");
 							return false;
@@ -95,7 +109,7 @@ game.enableMoveInput((event) => {
 							console.log("Illegal pawn movement");
 							return false;
 
-						} else if(!(game.getPiece(event.squareTo)).includes("b")) {
+						} else if (!(game.getPiece(event.squareTo)).includes("b")) {
 
 							console.log("Illegal pawn movement");
 							return false;
@@ -106,27 +120,27 @@ game.enableMoveInput((event) => {
 
 				};
 
-				if ((game.getPiece(event.squareTo)) == undefined) { 
+				if ((game.getPiece(event.squareTo)) == undefined) {
 
 					log_text.innerHTML = `Move accepeted: ${event.squareFrom}-${event.squareTo}`;
 
 					if (game.getPiece(event.squareFrom).includes("wp")) { //set counter to 0 if you move a pawn or +1 if any other piece
 
 						turns_white_text.innerHTML = `WMSLC: ${white_counter = 0}`;
-	
+
 					} else {
-						
+
 						turns_white_text.innerHTML = `WMSLC: ${white_counter += 1}`;
-					
+
 					};
-	
+
 					if (white_counter > 50) { //50 moves rule
 
 						log_text.innerHTML = `Stalemate - 50-move  rule`;
 						game.disableMoveInput();
-					
+
 					};
-					
+
 					return true;
 
 				} else if (game.getPiece(event.squareTo) === "wb") { //so you cant beat a white bishop
@@ -185,7 +199,7 @@ game.enableMoveInput((event) => {
 							console.log("Illegal pawn movement");
 							return false;
 
-						} else if(!(game.getPiece(event.squareTo)).includes("w")) {
+						} else if (!(game.getPiece(event.squareTo)).includes("w")) {
 
 							console.log("Illegal pawn movement");
 							return false();
@@ -201,20 +215,20 @@ game.enableMoveInput((event) => {
 					log_text.innerHTML = `Move accepeted: ${event.squareFrom}-${event.squareTo}`;
 
 					if (game.getPiece(event.squareFrom).includes("bp")) {
-	
+
 						turns_black_text.innerHTML = `BMSLC: ${black_counter = 0}`;
-	
+
 					} else {
 
 						turns_black_text.innerHTML = `BMSLC: ${black_counter += 1}`;
 
 					};
-					
+
 					if (black_counter > 5) {
 
 						log_text.innerHTML = `Stalemate - 50-move  rule`;
 						game.disableMoveInput();
-					
+
 					};
 
 					return true;
@@ -240,9 +254,9 @@ game.enableMoveInput((event) => {
 
 			};
 
-			case INPUT_EVENT_TYPE.moveCanceled:
+		case INPUT_EVENT_TYPE.moveCanceled:
 
-				log_text.innerHTML = `Canceled move`;
+			log_text.innerHTML = `Canceled move`;
 	};
 
 }, random_player_colours);
